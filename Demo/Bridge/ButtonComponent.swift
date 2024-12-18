b1498f4130697457dda229c3e350a9fa10ebb06f
Yaro Shm <yshmarov@gmail.com>
2024-12-18 23:55:42 +0100

    Update ButtonComponent.swift
    https://x.com/joemasilotti/status/1868076112301134084

--- Demo/Bridge/ButtonComponent.swift
+++ Demo/Bridge/ButtonComponent.swift
@@ -5,6 +5,14 @@ final class ButtonComponent: BridgeComponent {
     override class var name: String { "button" }
 
     override func onReceive(message: Message) {
+        if message.event == "connect" {
+            connect (via: message)
+        } else {
+            disconnect ()
+        }
+    }
+
+    private func connect (via message: Message) {
         guard let viewController else { return }
         addButton(via: message, to: viewController)
     }
@@ -16,13 +24,13 @@ final class ButtonComponent: BridgeComponent {
     private func addButton(via message: Message, to viewController: UIViewController) {
         guard let data: MessageData = message.data() else { return }
 
-       let image: UIImage?
+        let image: UIImage?
 
-       if let imageName = data.image {
-           image = UIImage(systemName: imageName)
-       } else {
-           image = nil
-       }
+        if let imageName = data.image {
+            image = UIImage(systemName: imageName)
+        } else {
+            image = nil
+        }
 
         let action = UIAction { [unowned self] _ in
             self.reply(to: "connect")
@@ -34,6 +42,11 @@ final class ButtonComponent: BridgeComponent {
         } else {
             viewController.navigationItem.leftBarButtonItem = item
         }
+    }
+
+    private func disconnect () {
+        let viewController = delegate.destination as? UIViewController
+        viewController?.navigationItem.rightBarButtonItem = nil
     }
 }
 
